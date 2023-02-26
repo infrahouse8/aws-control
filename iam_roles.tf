41d1809ccebafef277b7d442b8e9c136fbc5ba9b
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-02-26 14:07:51 -0800

    Create a TF user for S3
    The user will be repsonsible for maintaining Terraform state buckets

--- iam_roles.tf
+++ iam_roles.tf
@@ -24,3 +24,31 @@ resource "aws_iam_role_policy_attachment" "github-admin" {
   policy_arn = aws_iam_policy.TFAdminForGitHub.arn
   role       = aws_iam_role.github-admin.name
 }
+
+
+resource "aws_iam_role" "s3-admin" {
+  name = "s3-admin"
+  assume_role_policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Sid    = ""
+          Principal = {
+            AWS = [
+              aws_iam_user.aleks.arn,
+              aws_iam_user.tf_s3.arn,
+            ]
+          }
+        },
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "s3-admin" {
+  policy_arn = aws_iam_policy.TFAdminForS3.arn
+  role       = aws_iam_role.s3-admin.name
+}
--- users.tf
+++ users.tf
@@ -13,3 +13,12 @@ resource "aws_iam_user" "tf_github" {
 resource "aws_iam_access_key" "tf_github" {
   user = aws_iam_user.tf_github.name
 }
+
+
+resource "aws_iam_user" "tf_s3" {
+  name = "tf_s3"
+}
+
+resource "aws_iam_access_key" "tf_s3" {
+  user = aws_iam_user.tf_s3.name
+}
